9b8b7c6f4223c75ce8e4e2a6265ecead281e0ab4
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-08-31 14:14:46 +0300

    ШАГ. Д/з по сроку 31/08/2023 © Денис Заливко

---
+++ init.sql
@@ -0,0 +1,29 @@
+create database if not exists its_proj;
+
+create table if not exists Project (
+                                       name varchar(64),
+                                       date_start varchar(10),
+                                       manager_name varchar(32)
+);
+
+delete from Project where true;
+
+insert into Project (name, date_start, manager_name)
+VALUES
+    ('ProjectIT','03/05/2023','Alexandr S.'),
+    ('ProjectOwn','18/05/2023','Denis Z.');
+
+create table if not exists Company (
+                                       name varchar(64),
+                                       age integer,
+                                       email varchar(64)
+);
+
+delete from Company where true;
+
+insert into Company (name, age, email)
+VALUES
+    ('IT-Step', NULL, 'vitebsk.by@itstep.org'),
+    ('Рога и копыта', 99, 'horns-n-hoofs@faraway.club'),
+    ('Ferico - натяжные потолки', 18, 'info@ferico.by');
+
